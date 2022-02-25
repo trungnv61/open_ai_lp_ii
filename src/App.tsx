82fa4e2ca5e2d1d58194b70a11581dbb6a0a998d
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-25 21:49:18 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "./App.css";
-import Article from "./components/article/Article";
-import Brand from "./components/brand/Brand";
-import Cta from "./components/cta/Cta";
-import Feature from "./components/feature/Feature";
-import Navbar from "./components/navbar/Navbar";
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
 
 const App: React.FC = () => {
   return (
