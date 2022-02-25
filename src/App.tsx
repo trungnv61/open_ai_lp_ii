3589b01862e257e4af971e8e605806904da41560
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-25 22:42:08 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
 import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <p>Trung</p>
+    <div className="app">
+      <div className="gradient__bg"></div>
+      <Navbar />
     </div>
   );
 };
