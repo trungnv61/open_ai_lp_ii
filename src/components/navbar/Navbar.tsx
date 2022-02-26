fc0880d2400a9785f63269308ff594f04ef75d27
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-26 21:20:21 +0700

    init

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,11 @@ import "./navbar.css";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links"></div>
+    </div>
+  );
 };
 
 export default Navbar;
