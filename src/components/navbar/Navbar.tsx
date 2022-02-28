f6996f4921e44d5a0c1c508735b5838fc750b9ff
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-28 20:00:25 +0700

    init

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar: React.FC = () => {
@@ -8,6 +9,23 @@ const Navbar: React.FC = () => {
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#blog">Case Studies</a>
+          </p>
+          <p>
+            <a href="#home">Library</a>
+          </p>
         </div>
       </div>
     </div>
