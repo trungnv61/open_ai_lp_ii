e626066ea6ecee2ad615eb24a6a45a10fb35d2b7
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-02-28 19:19:03 +0700

    init

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,7 +5,11 @@ import "./navbar.css";
 const Navbar: React.FC = () => {
   return (
     <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links"></div>
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
   );
 };
