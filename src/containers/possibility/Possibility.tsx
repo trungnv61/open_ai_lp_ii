315fd28b416eb3ef9acb05d8f18ea2d7adc1a07d
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-03-08 10:43:27 +0700

    init

--- src/containers/possibility/Possibility.tsx
+++ src/containers/possibility/Possibility.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./possibility.css";
 const Possibility: React.FC = () => {
-  return <div>Possibility</div>;
+  return <div className="gpt3__possibility section__padding"></div>;
 };
 
 export default Possibility;
