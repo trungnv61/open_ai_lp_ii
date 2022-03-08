0e0053ed3b731c5d7eab6326fc1bcd25ae61ba1d
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-03-08 18:51:09 +0700

    init

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import from '../../assets/'
 
-const Article: React.FC = () => {
-  return <div>Article</div>;
+
+const Article: React.FC = ({imgUrl}) => {
+  return (
+    <div className="gpt3__blog-container__article">
+      Article
+      <div className="gpt3__blog-container__article-image">
+        <img src={imgUrl} alt="blogImage" />
+        </div>
+    </div>
+  );
 };
 
 export default Article;
---
+++ src/containers/blog/import.js
@@ -0,0 +1,5 @@
+import blog1 from "../../assets/blog01.png";
+import blog2 from "../../assets/blog02.png";
+import blog3 from "../../assets/blog03.png";
+import blog1 from "../../assets/blog01.png";
+import blog1 from "../../assets/blog01.png";
