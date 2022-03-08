4e99a9d7af83cfa53635cc53afcd01f9c3396783
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-03-08 16:09:24 +0700

    init

--- src/components/cta/Cta.tsx
+++ src/components/cta/Cta.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./cta.css";
 const Cta: React.FC = () => {
-  return <div>Cta</div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-container">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
